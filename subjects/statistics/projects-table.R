3c444cce413576516e28ef44ff22838e2f4d662e
jose <jcmcampos@fc.ul.pt>
2021-12-13 10:12:06 +0000

    Added script to automatically generate a latex table with a quick summary of all quantum projects.

---
+++ subjects/statistics/projects-table.R
@@ -0,0 +1,44 @@
+# ------------------------------------------------------------------------------
+# This scripts generates a projects' table summary.
+#
+# Usage:
+# Rscript projects-table.R <tex_file_path, e.g., projects.tex>
+#
+# ------------------------------------------------------------------------------
+
+library('this.path') # install.packages('this.path')
+source(paste(this.dir(), '/util.R', sep=''))
+
+args = commandArgs(trailingOnly=TRUE)
+if (length(args) < 1) {
+  stop('USAGE: projects-table.R <tex_file_path, e.g., projects.tex>')
+}
+
+OUTPUT_FILE_PATH <- args[1]
+
+# Load data
+DATA_FILE_PATH <- paste(this.dir(), '/../data/generated/bugs-in-quantum-computing-platforms.csv', sep='')
+df <- set_programming_languages(read.table(DATA_FILE_PATH, header=TRUE, stringsAsFactors=FALSE))
+print(head(df))
+
+unlink(OUTPUT_FILE_PATH)
+sink(OUTPUT_FILE_PATH, append=FALSE, split=TRUE)
+
+cat('\\begin{tabular}{@{\\extracolsep{\\fill}} llrrr} \\toprule\n', sep='')
+cat('\\multicolumn{1}{c}{Project} & \\multicolumn{1}{c}{Language} & \\multicolumn{1}{c}{\\# Bugs} & \\multicolumn{1}{c}{\\# Classical} & \\multicolumn{1}{c}{\\# Quantum} \\\\\n\\midrule\n', sep='')
+
+for (project_full_name in sort(unique(df$'project_full_name'))) {
+  mask <- df$'project_full_name' == project_full_name
+  cat(project_full_name, ' & ',
+      replace_string(replace_string(unique(df$'languages'[mask]), '#', '\\\\#'), ' ', ', '), ' & ',
+      nrow(df[mask, ]), ' & ',
+      nrow(df[mask & df$'type' == 'Classical', ]), ' & ',
+      nrow(df[mask & df$'type' == 'Quantum', ]), ' \\\\\n', sep='')
+}
+
+cat('\\bottomrule\n', sep='')
+cat('\\end{tabular}\n', sep='')
+
+sink()
+
+# EOF
--- subjects/statistics/util.R
+++ subjects/statistics/util.R
@@ -5,4 +5,27 @@
 library('this.path') # install.packages('this.path')
 source(paste(this.dir(), '/../../utils/statistics/util.R', sep=''))
 
+set_programming_languages <- function(df) {
+  df$'languages' <- NA
+  df$'languages'[df$'project_full_name' == 'PennyLaneAI/pennylane']        <- 'Python'
+  df$'languages'[df$'project_full_name' == 'ProjectQ-Framework/ProjectQ']  <- 'C++ Python'
+  df$'languages'[df$'project_full_name' == 'QE-Lab/OpenQL']                <- 'C++ Python'
+  df$'languages'[df$'project_full_name' == 'Qiskit/qiskit-aer']            <- 'Python'
+  df$'languages'[df$'project_full_name' == 'Qiskit/qiskit-ignis']          <- 'Python'
+  df$'languages'[df$'project_full_name' == 'Qiskit/qiskit-terra']          <- 'Python'
+  df$'languages'[df$'project_full_name' == 'aspuru-guzik-group/tequila']   <- 'Python'
+  df$'languages'[df$'project_full_name' == 'aws/amazon-braket-sdk-python'] <- 'Python'
+  df$'languages'[df$'project_full_name' == 'dwavesystems/dwave-system']    <- 'Python'
+  df$'languages'[df$'project_full_name' == 'eclipse/xacc']                 <- 'C++ C Python'
+  df$'languages'[df$'project_full_name' == 'microsoft/QuantumLibraries']   <- 'Q# C#'
+  df$'languages'[df$'project_full_name' == 'microsoft/qsharp-compiler']    <- 'C# F# Q#'
+  df$'languages'[df$'project_full_name' == 'microsoft/qsharp-runtime']     <- 'C# C++ Q#'
+  df$'languages'[df$'project_full_name' == 'quantumlib/Cirq']              <- 'Python'
+  df$'languages'[df$'project_full_name' == 'qulacs/qulacs']                <- 'C++ C'
+  df$'languages'[df$'project_full_name' == 'rigetti/pyquil']               <- 'Python'
+  df$'languages'[df$'project_full_name' == 'unitaryfund/mitiq']            <- 'Python'
+  df$'languages'[df$'project_full_name' == 'xanaduai/strawberryfields']    <- 'Python'
+  return(df)
+}
+
 # EOF

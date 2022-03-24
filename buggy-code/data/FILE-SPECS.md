19a7464d667ddf677273607881700bb111118180
jose <jcmc@fe.up.pt>
2022-03-24 15:00:25 +0000

    Fixed columns order and added documentation.

---
+++ buggy-code/data/FILE-SPECS.md
@@ -0,0 +1,26 @@
+# Specifications of data files
+
+## [buggy-code-data.csv](generated/buggy-code-data.csv)
+
+- `project_full_name`: Full name of a GitHub project, i.e., organization + "/" + project-name
+  * factor, e.g., "Qiskit/qiskit-ignis"
+
+- `buggy_commit_hash`: git commit hash of the buggy source code
+  * factor, e.g., "ec1b4ce759f1fb8ba0242dd6c4a309fa1b586666"
+
+- `bug_id`: Bug id as original identified in the [Bugs in Quantum Computing Platforms: An Empirical Study](https://arxiv.org/abs/2110.14560)
+  * factor: e.g., "1", "1909-5"
+
+- `bug_type`: bug type
+  * factor: "Classical" or "Quantum"
+
+- `buggy_file_path`: relative path of the buggy file
+  * factor, e.g., "qiskit_ignis/tomography/fitters/cvx_fit.py"
+
+- `buggy_line_number`: line number of a buggy line
+  * numerical
+
+- `buggy_component`: buggy comment in `buggy_line_number` as described in the [Python's AST](https://docs.python.org/3/library/ast.html) documentation
+  * factor, e.g., "Call", "Assignment", etc
+
+Note that the same `buggy_line_number` in the same `buggy_file_path` of a `project`-`bug_id` bug could have more than one `buggy_component`.
--- buggy-code/scripts/compute-buggy-code-data.sh
+++ buggy-code/scripts/compute-buggy-code-data.sh
@@ -60,7 +60,7 @@ done
 [ -s "$BUGS_FILE_PATH" ]     || die "[ERROR] $BUGS_FILE_PATH does not exist or it is empty!"
 # Remove the output_file_path (if any) and create a new one
 rm -f "$OUTPUT_FILE_PATH"
-echo "project_full_name,bug_id,bug_type,buggy_commit_hash,buggy_file_path,buggy_line_number,buggy_component" > "$OUTPUT_FILE"
+echo "project_full_name,buggy_commit_hash,bug_id,bug_type,buggy_file_path,buggy_line_number,buggy_component" > "$OUTPUT_FILE"
 
 # ------------------------------------------------------------------------- Main
 
@@ -117,7 +117,7 @@ while read -r item; do
     while read -r buggy_components_per_buggy_line; do
       buggy_line_number=$(echo "$buggy_components_per_buggy_line" | cut -f1 -d',')
       buggy_component=$(echo "$buggy_components_per_buggy_line" | cut -f2 -d',')
-      echo "$project_full_name,$bug_id,$bug_type,$buggy_commit_hash,$buggy_file_path,$buggy_line_number,$buggy_component" >> "$OUTPUT_FILE" || die "[ERROR] Failed to append data to the $OUTPUT_FILE file!"
+      echo "$project_full_name,$buggy_commit_hash,$bug_id,$bug_type,$buggy_file_path,$buggy_line_number,$buggy_component" >> "$OUTPUT_FILE" || die "[ERROR] Failed to append data to the $OUTPUT_FILE file!"
     done < <(tail -n +2 "$tmp_buggy_components_file")
   done < <(git --git-dir="$PROJECTS_REPOSITORIES_DIR/$project_full_name" diff --no-ext-diff --binary --name-only "$buggy_commit_hash" "$fix_commit_hash" | grep ".py$")
 done < <(tail -n +2 "$BUGS_FILE_PATH")

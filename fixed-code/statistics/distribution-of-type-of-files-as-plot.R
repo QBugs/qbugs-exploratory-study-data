9a722d91e6c0d831db9e953c5d99a05e3d50bab5
jose <jcmc@fe.up.pt>
2022-05-25 12:06:24 +0100

    Updated R script to automatically parse type of each file.

--- fixed-code/statistics/distribution-of-type-of-files-as-plot.R
+++ fixed-code/statistics/distribution-of-type-of-files-as-plot.R
@@ -3,7 +3,7 @@
 #
 # Usage:
 #   Rscript distribution-of-type-of-files-as-plot.R
-#     <input data file, e.g., ../data/generated/type-of-file.csv>
+#     <input data file, e.g., ../data/generated/edited-files.csv>
 #     <output pdf file, e.g., distribution-of-type-of-files-as-plot.pdf>
 # ------------------------------------------------------------------------------
 
@@ -20,7 +20,7 @@ library('effsize')
 
 args = commandArgs(trailingOnly=TRUE)
 if (length(args) != 2) {
-  stop('USAGE: Rscript distribution-of-type-of-files-as-plot.R <input data file, e.g., ../data/generated/type-of-file.csv> <output pdf file, e.g.,  distribution-of-type-of-files-as-plot.pdf>')
+  stop('USAGE: Rscript distribution-of-type-of-files-as-plot.R <input data file, e.g., ../data/generated/edited-files.csv> <output pdf file, e.g.,  distribution-of-type-of-files-as-plot.pdf>')
 }
 
 # Args
@@ -32,13 +32,24 @@ OUTPUT_FILE <- args[2]
 # Load data
 df <- load_CSV(INPUT_FILE)
 
+# Set (default) type of file
+df$'file_type' <- 'Unknown'
+# Rules to set type of each file
+df$'file_type'[grep('CODEOWNERS|Quil.g4$|.md$|.rst$', df$'file_path')]                                   <- 'Documentation File'
+df$'file_type'[grep('.cfg$|.ini$|.toml$', df$'file_path')]                                               <- 'Configuration File'
+df$'file_type'[grep('.cpp$|.cs$|.fs$|.h$|.hpp$|.ll$|.py$|.qisa$|.qs$', df$'file_path')]                  <- 'Source Code File'
+df$'file_type'[grep('.csproj$|.fsproj$|makefile|.ps1$|Simulation.sln|Sdk.targets|.rs$', df$'file_path')] <- 'Build File'
+df$'file_type'[grep('.ipynb$|.sh$', df$'file_path')]                                                     <- 'Script File'
+df$'file_type'[grep('.json$|TrialResult.repr|.yaml$|.yml$', df$'file_path')]                             <- 'Data File'
+df$'file_type'[grep('.txt$', df$'file_path')]                                                            <- 'Text File'
+
 # Aggregate data
 df$'count' <- 1
-agg_count <- aggregate(x=count ~ bug_id + bug_type + type_of_file, data=df, FUN=sum)
+agg_count <- aggregate(x=count ~ bug_id + bug_type + file_type, data=df, FUN=sum)
 
 # Remove any existing output file and create a new one
 unlink(OUTPUT_FILE)
-pdf(file=OUTPUT_FILE, family='Helvetica', width=10, height=9)
+pdf(file=OUTPUT_FILE, family='Helvetica', width=7, height=4)
 # Add a cover page to the output file
 plot_label('Distributions')
 
@@ -51,7 +62,7 @@ for (bug_type in unique(df$'bug_type')) {
 
   for (keep_order in c(TRUE, FALSE)) {
     plot_label(paste('UpSetR (overall)', '\n', 'keep.order=', keep_order, '\n', bug_type, sep=''))
-    a <- dcast(df[df$'bug_type' == bug_type, ], ... ~ type_of_file, value.var='type_of_file', fun.aggregate=length)
+    a <- dcast(df[df$'bug_type' == bug_type, ], ... ~ file_type, value.var='file_type', fun.aggregate=length)
     a <- a[ , which(colnames(a) %!in% columns) ]
     p <- upset(a, sets=colnames(a), order.by=c('freq'), nintersects=50, keep.order=keep_order, set_size.show=TRUE,
                mb.ratio=c(0.40, 0.60), #point.size=3.5, line.size=1.1,
@@ -62,8 +73,8 @@ for (bug_type in unique(df$'bug_type')) {
     print(p)
     
     plot_label(paste('UpSetR (agg by bug)', '\n', 'keep.order=', keep_order, '\n', bug_type, sep=''))
-    a <- dcast(aggregate(x=. ~ bug_id + type_of_file, data=df[df$'bug_type' == bug_type, ], FUN=length),
-               bug_id ~ type_of_file, value.var='type_of_file', fun.aggregate=length)
+    a <- dcast(aggregate(x=. ~ bug_id + file_type, data=df[df$'bug_type' == bug_type, ], FUN=length),
+               bug_id ~ file_type, value.var='file_type', fun.aggregate=length)
     a <- a[ , which(colnames(a) %!in% c('bug_id')) ]
     p <- upset(a, sets=colnames(a), order.by=c('freq'), nintersects=NA, keep.order=keep_order, set_size.show=TRUE,
                mb.ratio=c(0.40, 0.60), #point.size=3.5, line.size=1.1,
